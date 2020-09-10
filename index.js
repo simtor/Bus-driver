6378e97b09e318c753442ab433f68ee933ef9211
simtor <simone.ram@ada.ac.uk>
2020-09-10 11:27:21 +0100

    testing

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let drivers = ["Harman","James","Jessica", "Kacper",'Roheel', "Simone", "Sukh", "Tim"]
 
 // setTimeout(driversTimer, 1500);
-setInterval(driversTimer, 300000);
+// setInterval(driversTimer, 300000);
+setInterval(driversTimer, 1000);
 
-console.log("Simone Ram")
+console.log("Driver selector started: ")
 
 function driversTimer(){
     const driverSelector = Math.floor(Math.random() * drivers.length);
     console.log(drivers[driverSelector]);
 }
-
 driversTimer()
 
 
