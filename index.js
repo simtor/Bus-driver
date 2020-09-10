4bcd10b4b78947f8ee777b6508c6cf90d82530c6
simtor <simone.ram@ada.ac.uk>
2020-09-10 09:43:31 +0100

    create a function for drivers

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let drivers = ["Harman","James","Jessica", "Kacper",'Roheel', "Simone", "Sukh", "Tim"]
 
+function driversTimer(){
+    const driverSelector = Math.floor(Math.random() * drivers.length);
+    console.log(drivers[driverSelector]);
+}
 
-const driverSelector = Math.floor(Math.random() * drivers.length);
-console.log(drivers[driverSelector]);
+
+
+
