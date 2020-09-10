4ea389cd7e7d79e52dcf763d9a37e976a4e9a50a
simtor <simone.ram@ada.ac.uk>
2020-09-10 09:53:44 +0100

    timer function works

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let drivers = ["Harman","James","Jessica", "Kacper",'Roheel', "Simone", "Sukh", "Tim"]
 
+setTimeout(driversTimer, 1500);
+
+console.log("Simone Ram")
+
 function driversTimer(){
     const driverSelector = Math.floor(Math.random() * drivers.length);
     console.log(drivers[driverSelector]);
@@ -7,4 +11,12 @@ function driversTimer(){
 
 
 
+
+// setTimeout(bye, 3000);
+
+// console.log("hello");
+
+// function bye(){
+//     console.log("goodbye")
+// }
 
