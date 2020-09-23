5e5ed88eca34cf7adf183bb8b437186b848a9a32
simtor <simone.ram@ada.ac.uk>
2020-09-23 16:54:42 +0100

    incomplete

--- index.js
+++ index.js
@@ -10,14 +10,37 @@ console.log("Driver selector started: ")
 
 function driversTimer(){
     const driverSelector = Math.floor(Math.random() * drivers.length);
-    notSameDriver.push(driverSelector);
+    // notSameDriver.push(driverSelector);
     console.log(drivers[driverSelector]);
     // console.log(notSameDriver)
 }
+
+// function notDouble(driverSelector){
+//     let foundDouble = notSameDriver.includes(driverSelector)
+//     if (foundDouble){
+//         console.log("You need to change");
+//         driversTimer()
+//     }else{
+//         console.log("failure");
+//     }
+// }
+
+// notDouble();
+
 driversTimer()
 
 
-
+// function driversTimer(){
+//     let driverSelector;
+//     let lastrandom;
+//     while (driverSelector  === lastrandom){
+//         driverSelector = Math.floor(Math.random() * drivers.length);
+//         notSameDriver.push(driverSelector);
+//         console.log(drivers[driverSelector]);
+//         console.log(notSameDriver)
+//     }
+//     lastrandom = driverSelector;
+// }
 
 // setTimeout(bye, 3000);
 
